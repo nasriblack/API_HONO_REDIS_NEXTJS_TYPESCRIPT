d0e58639607bbd22d24a940233755873f1519e7a
Nasereddine Lakhal <naserlakhal@gmail.com>
2024-10-20 11:53:08 +0100

    addind shadcn , complete the ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import {
+  Command,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -14,6 +21,11 @@ export default function Home() {
       if (!input) return setSearchResults(undefined);
 
       const res = await fetch(`/api/search?q=${input}`);
+      const data = (await res.json()) as {
+        results: string[];
+        duration: number;
+      };
+      setSearchResults(data);
     };
 
     fetchData();
@@ -24,17 +36,51 @@ export default function Home() {
   }, [input]);
 
   return (
-    <>
-      <input
-        className="text-zinc-900"
-        value={input}
-        type="text"
-        name=""
-        id=""
-        onChange={(e) => {
-          setInput(e.target.value);
-        }}
-      />
-    </>
+    <main className="h-screen w-screen grainy">
+      <div className="flex flex-col gap-6 items-center pt-32 duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5">
+        <h1 className="text-5xl tracking-tight font-bold">SpeedSearch ⚡</h1>
+        <p className="text-zinc-600 text-lg max-w-prose text-center">
+          A high-performance API built with Hono, Next.js and Cloudflare. <br />{" "}
+          Type a query below and get your results in miliseconds.
+        </p>
+        <div className="max-w-md w-full">
+          <Command>
+            <CommandInput
+              value={input}
+              onValueChange={setInput}
+              placeholder="Search countries..."
+              className="placeholder:text-zinc-500"
+            />
+            <CommandList>
+              {searchResults?.results ? (
+                <>
+                  <CommandGroup heading="Results">
+                    {searchResults.results.map((result) => (
+                      <CommandItem
+                        key={result}
+                        value={result}
+                        onSelect={setInput}
+                      >
+                        {result}
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </>
+              ) : null}
+            </CommandList>
+          </Command>
+          {searchResults?.results ? (
+            <>
+              <div className="h-px w-full bg-zin-200" />
+
+              <p className="p-2 text-xs text-zinc-500">
+                Found {searchResults.results.length} results in{" "}
+                {searchResults?.duration.toFixed(0)}ms
+              </p>
+            </>
+          ) : null}
+        </div>
+      </div>
+    </main>
   );
 }
